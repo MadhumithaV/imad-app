153231d571e3e69def1d5dae0c6e65ecde9723da
MadhumithaV <30660160+MadhumithaV@users.noreply.github.com>
2017-08-10 06:13:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,19 @@ button.onclick = function(){
 
     
 };
+
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+  
+  var names=['name1','name2','name3','name4'];
+  var list='';
+  for(i=0;i<name.length;i++){
+      list+= '<li>' +names[i]+ '</li>';
+  }
+  
+  var ul=document.getElementById('namelist');
+  ul.innerHTML=list;
+    
+};
