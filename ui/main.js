58361958a355b0d76b90dbfcc9e86596f962256e
MadhumithaV <30660160+MadhumithaV@users.noreply.github.com>
2017-08-10 06:16:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,7 +32,7 @@ submit.onclick=function(){
   
   var names=['name1','name2','name3','name4'];
   var list='';
-  for(i=0;i<name.length;i++){
+  for(var i=0;i<name.length;i++){
       list+= '<li>' +names[i]+ '</li>';
   }
   
