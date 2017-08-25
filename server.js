debdb47023c27f3a7bd7c19d7d57d8e56342fa85
MadhumithaV <30660160+MadhumithaV@users.noreply.github.com>
2017-08-25 07:48:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
 
+var.config={
+    user:'meetuaviation',
+    database:'meetuaviation',
+    
+    
+    
+    
+    };
 var app = express();
 app.use(morgan('combined'));
 
